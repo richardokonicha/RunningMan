0b9718d45f792f85e331f6d33390e2591467513d
Richard Okonicha <richardokonicha@gmail.com>
2020-08-23 10:53:43 +0100

    support for single string

--- runner.py
+++ runner.py
@@ -2,7 +2,12 @@ from config import channel_link
 from telethon.tl.functions.channels import InviteToChannelRequest
 from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
 import traceback
+import telethon
 from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.messages import AddChatUserRequest
+from telethon.tl.types import InputPeerUser
+
+
 
 async def joinner(channel, bot_client):
     join = await bot_client(JoinChannelRequest(channel)) 
@@ -16,6 +21,21 @@ async def runner(user, bot_client):
         ))
         username = user.username
         print(f"RunningMan added {username}")
+
+    except telethon.errors.rpcerrorlist.UserIdInvalidError:
+          user = InputPeerUser(user.id, user.access_hash)
+          invite = await bot_client(InviteToChannelRequest(
+            channel_link,
+            [user]
+        ))
+
+
+        # await bot_client(AddChatUserRequest(
+        #     channel_link,
+        #     user,
+        #     fwd_limit=10  # Allow the user to see the 10 last messages
+        # ))
+
     except PeerFloodError:
         print("Getting Flood Error from telegram. Script is stopping now. Please try again after some time.")
     except UserPrivacyRestrictedError:
--- runningman.py
+++ runningman.py
@@ -8,18 +8,14 @@ import random
 from telethon.sessions import StringSession
 from telethon.tl.functions.channels import JoinChannelRequest
 from db import FetchGroup, FetchSession
+import telethon
 
 
-# sessionString = [
-#     {"string":"1BJWap1sBuxVAQiLBVM1CaYhIBUJ3GaEDD5bdXj3gCFpuUaNGXBmwWKQfRsXOmjZAHzDcLXzUohw2A0X203XxCH2I8JZsPuRmkrMvstAJidx40OGm1-RJZWXHoMsigLVMnkisY5o22a2iYgmrjsl_e0hb6TodNkTcvk9xpfkQZxwCJoKTEJuOHumeSCvx1cs_scq_tMOv3UUF8xKtYNLMFWRDtmjuP1rBiM6W2h9vZU0eLFDb_-mAGQv-yXtqnBQ7b3yqq3dLlD7-Q4gsO4WeXnHprVy5UGw8I5fA69-otTKy8mV16Bps0N3VAoiA79AHy63T18cBCRSZJ1dQsFnpBLqOObYFsPc=", "index": 0},
-#     {"string": "1BJWap1wBu2KUNs72uH17ooCoaMHg8kfagmf2q-Vk9EsJh3DLEFXbRKw0fsFf9LcUt4z5keiF1ack2imGf5iHIMJFimjkas3472COmgMRRX3cdy2LAIHFHFP76RICquy9ieT2UtDmyvCjRZo6sg8CMQ0q5o4W3uqt9jmdPD7OXsHSGmb91wq8zXyfOqRcFo_aKMY3omlHDfDWPR8GujO5CRDs3yddC0GfUqncMsg5ag8Pp7BRY72pSAR8-xGh33hHtg0kR6T4n4uXVmDjIzDz2sHTOL_8QHESYi1933FGmshv6jMjOpfac-SwpVE4dL39y_hM9Cb7IARgBur9Jmmt2p1J3AKtCsc=","index": 0},
-#     {"string": "1BJWap1wBu2gfrjGz_C1d2vfy0yqB0OVkmqvxm_A8QOc4BDpPEqeFB4_jySYMBeWJfv_MBOz91-A4CHxQLVnNBWASXdbVrazuInOaj98TxzhPXMBCd1oHqGwXH2Q1vqIQH0g6F7Ni7r9LH-IP0-11tK5W11WJUIp6_Op_o4QUROymIaserhQFbMqxf_XMoRR4PvgK_E7kz0NsPwPlvFaGY5XiMQ4uLG222gYAvzSOiSfJsuIhdMW_Pwb3xaL2hV3FwGlvW9fGJXdR3VSIL_2qGY9L0fJGju90FlYlc4btpFeYH72g07sZ_P9aKb81tNUTbeKFU3hePtQ3ltQqYXKRKBjHdISOEDE=", "index": 0}
-#     ]
 
 sessionString = FetchSession.objects.all()
 
 n = 0
-max_add = 1
+max_add = 50
 
 
 
@@ -31,6 +27,7 @@ def whistle(bot_client, catch):
         me = bot_client.get_me()
         run = bot_client.loop.run_until_complete(
             runner(user, bot_client))
+        # if run = None
         number += 1
         if number >= max_add:
             print(f"Reached {max_add} cooling down")
@@ -51,15 +48,19 @@ catch = bot_client.loop.run_until_complete(
 fetch_group = FetchGroup.objects(group_id= group.id).first()
 answer_index = fetch_group["index_remain"]
 
-
+sessionString = ["1BJWap1sBuxVAQiLBVM1CaYhIBUJ3GaEDD5bdXj3gCFpuUaNGXBmwWKQfRsXOmjZAHzDcLXzUohw2A0X203XxCH2I8JZsPuRmkrMvstAJidx40OGm1-RJZWXHoMsigLVMnkisY5o22a2iYgmrjsl_e0hb6TodNkTcvk9xpfkQZxwCJoKTEJuOHumeSCvx1cs_scq_tMOv3UUF8xKtYNLMFWRDtmjuP1rBiM6W2h9vZU0eLFDb_-mAGQv-yXtqnBQ7b3yqq3dLlD7-Q4gsO4WeXnHprVy5UGw8I5fA69-otTKy8mV16Bps0N3VAoiA79AHy63T18cBCRSZJ1dQsFnpBLqOObYFsPc="]
 for SES in sessionString:
     print("--------------------")
 
     bot_client.disconnect()
-    string = sessionString[n]["session"]
+    string = SES
+
     bot_client = TelegramClient(StringSession(string), api_id, api_hash)
     bot_client.connect()
-    bot_client.loop.run_until_complete(joinner(groupLink, bot_client))
+    try:
+        bot_client.loop.run_until_complete(joinner(groupLink, bot_client))
+    except telethon.errors.rpcerrorlist.InviteHashInvalidError:
+        pass
 
 
     me = bot_client.get_me()
